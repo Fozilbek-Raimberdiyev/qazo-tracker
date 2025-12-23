8184506c041453ee5c587dbe984910989592b7d1
Fozilbek Raimberdiyev <fl.raimberdiyev@gmail.com>
2025-12-24 00:59:24 +0500

    feat(pwa setup):finish

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,13 +3,78 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // yangi versiya chiqsa avtomatik yangilanadi
+      injectRegister: 'auto',     // avtomatik service worker ro'yxatdan o'tkazadi
+
+      manifest: {
+        name: 'Qazo Tracker',
+        short_name: 'NamozApp',
+        description: 'Namoz va roʻza qazo hisoblagich',
+        theme_color: '#10b981',     // emerald-500 misol uchun
+        background_color: '#ffffff',
+        display: 'standalone',      // brauzer UI siz chiqadi
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 yil
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'gstatic-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+            },
+          },
+        ],
+      },
+
+      devOptions: {
+        enabled: true, // developmentda ham PWA ni sinab ko'rish uchun
+      },
+    }),
   ],
   resolve: {
     alias: {
